af280318cd8ed5919b4eb7502ee1e1568d3bb9f5
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-03-24 18:16:51 +0300

    fix: remove GasInfo's price field from admin panel

--- wish_swap/networks/admin.py
+++ wish_swap/networks/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from wish_swap.networks.models import GasInfo
 
 
-class GasInfoAdmin(admin.ModelAdmin):
-    readonly_fields = ('price',)
-
-
-admin.site.register(GasInfo, GasInfoAdmin)
+admin.site.register(GasInfo)
